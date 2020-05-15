1576a91d6cfdea9881566036facbc2222d7f92aa
Mark Fog <mp@itoperators.dk>
2020-05-15 14:51:54 +0200

    Doing Alert

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { CheckboxComponent } from './checkbox/checkbox.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {AuthInterceptor} from './auth/auth.interceptor';
+import { AlertComponent } from './alert/alert.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import {AuthInterceptor} from './auth/auth.interceptor';
     HeaderComponent,
     FooterComponent,
     SignupFormComponent,
-    CheckboxComponent
+    CheckboxComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
