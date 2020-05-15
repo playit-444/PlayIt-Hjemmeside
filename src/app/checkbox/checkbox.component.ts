56a4f438fe4671a8188f8260e01f06e3373514b0
SkraptZz <36505363+SkraptZz@users.noreply.github.com>
2020-05-15 10:02:49 +0200

    Checkbox with parameters
    Checkboxes tager nu id og text som parameter.

--- src/app/checkbox/checkbox.component.ts
+++ src/app/checkbox/checkbox.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { faSquare, faCheckSquare } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
@@ -7,14 +7,17 @@ import { faSquare, faCheckSquare } from '@fortawesome/free-regular-svg-icons';
   styleUrls: ['./checkbox.component.css']
 })
 export class CheckboxComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
   faSquare = faSquare;
   faCheckSquare = faCheckSquare;
   public checkboxChecked = false;
+  @Input() cbId: number;
+  @Input() cbText: string;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
   
   changeCheckbox() {
     if (this.checkboxChecked) {
