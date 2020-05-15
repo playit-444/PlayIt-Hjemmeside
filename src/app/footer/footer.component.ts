34c115599e1964b07bf413c0df44f2fa3e68f196
Mark Fog <mp@itoperators.dk>
2020-05-15 13:29:18 +0200

    Social Media Added to footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faFacebookSquare, faTwitterSquare, faLinkedin, faInstagramSquare } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+
+  facebook = faFacebookSquare;
+  twitter = faTwitterSquare;
+  linkedin = faLinkedin;
+  instagram = faInstagramSquare;
 
   constructor() { }
 
