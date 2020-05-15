5b02e0f63b0d8c415e75cb01819546d5595ecdb6
Mark Fog <mp@itoperators.dk>
2020-05-15 11:04:44 +0200

    Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-<<<<<<< HEAD
 import { FooterComponent } from './footer/footer.component';
-=======
 import { SignupFormComponent } from './signup-form/signup-form.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CheckboxComponent } from './checkbox/checkbox.component';
 
->>>>>>> 56a4f438fe4671a8188f8260e01f06e3373514b0
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-<<<<<<< HEAD
-    FooterComponent
-=======
+    FooterComponent,
     SignupFormComponent,
     CheckboxComponent
->>>>>>> 56a4f438fe4671a8188f8260e01f06e3373514b0
   ],
   imports: [
     BrowserModule,
