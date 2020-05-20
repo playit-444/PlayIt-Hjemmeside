e10ee023d1d4ee614079f9035ce2cc0ae97cae05
SkraptZz <36505363+SkraptZz@users.noreply.github.com>
2020-05-20 19:52:23 +0200

    Lobby player components

--- src/app/game-page/game-page.module.ts
+++ src/app/game-page/game-page.module.ts
@@ -4,8 +4,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableComponent } from './table-selection/table/table.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
 import { GamePageRoutingModule } from './game-page-routing.module';
+import { PlayerComponent } from './lobby/player/player.component';
 
 
 @NgModule({
@@ -13,6 +13,7 @@ import { GamePageRoutingModule } from './game-page-routing.module';
     TableSelectionComponent,
     LobbyComponent,
     TableComponent,
+    PlayerComponent,
   ],
   imports: [
     CommonModule,
