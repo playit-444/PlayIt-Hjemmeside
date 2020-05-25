47b76a5a09fa6e342aaa2ebd589bfb0a3286186e
Christoffer <42372704+Cp-Itoperators@users.noreply.github.com>
2020-05-25 22:10:55 +0200

    Updated socket service

--- src/app/services/web-socket.service.ts
+++ src/app/services/web-socket.service.ts
@@ -25,7 +25,7 @@ export class WebSocketService {
 
 
     this.subject.subscribe(
-      msg => console.log('message received: ' + msg), // Called whenever there is a message from the server.
+      msg => console.log(msg), // Called whenever there is a message from the server.
       err => console.log(err), // Called if at any point WebSocket API signals some kind of error.
       () => console.log('complete') // Called when connection is closed (for whatever reason).
     );
