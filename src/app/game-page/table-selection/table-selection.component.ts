90d0162395b3dfe1945257b20fbbc675d3dda0e1
Christoffer <42372704+Cp-Itoperators@users.noreply.github.com>
2020-05-25 09:59:39 +0200

    Session send

--- src/app/game-page/table-selection/table-selection.component.ts
+++ src/app/game-page/table-selection/table-selection.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { GameService } from 'src/app/services/game.service';
-import { Table } from 'src/app/models/table';
-import { Game } from 'src/app/models/game';
+import {Component, OnInit, Input} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {GameService} from 'src/app/services/game.service';
+import {Table} from 'src/app/models/table';
+import {Game} from 'src/app/models/game';
+import {WebSocketService} from "../../services/web-socket.service";
 
 @Component({
   selector: 'app-table-selection',
@@ -16,65 +17,62 @@ export class TableSelectionComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private gameService: GameService,
-    ) { }
+    private webSocketService: WebSocketService
+  ) {
+  }
 
   ngOnInit(): void {
-
-
     this.GetGame();
+    this.webSocketService.sendToServer();
 
-
-      const table: Table = {
-        roomID: '1',
-        name: 'string',
-        maxUsers: 2,
-        currentUsers: 1,
-        private: false
-      }
-      /*this.tables.push(table);
-      this.tables.push(table);
-      this.tables.push(table);
-      this.tables.push(table);
-      this.tables.push(table);
-      this.tables.push(table);
-      this.tables.push(table);
-      this.tables.push(table);
-      this.tables.push(table);*/
+    const table: Table = {
+      roomID: '1',
+      name: 'string',
+      maxUsers: 2,
+      currentUsers: 1,
+      private: false
+    }
+    /*this.tables.push(table);
+    this.tables.push(table);
+    this.tables.push(table);
+    this.tables.push(table);
+    this.tables.push(table);
+    this.tables.push(table);
+    this.tables.push(table);
+    this.tables.push(table);
+    this.tables.push(table);*/
 
   }
 
   GetGame() {
     this.route
-    .queryParams
-    .subscribe(params => {
-      const gameID = params.gameID;
+      .queryParams
+      .subscribe(params => {
+        const gameID = params.gameID;
 
-      if(gameID != null) {
-        this.gameService.GetGameType(gameID)
-        .subscribe(success => {
-          this.game = success;
-          this.GetTables(success.gameTypeId);
-        },
-        err => {
-          console.log(err.error);
-        });
-      }
-    });
+        if (gameID != null) {
+          this.gameService.GetGameType(gameID)
+            .subscribe(success => {
+                this.game = success;
+                this.GetTables(success.gameTypeId);
+              },
+              err => {
+                console.log(err.error);
+              });
+        }
+      });
   }
 
 
-
-
-
   GetTables(id: number) {
-      if (id != null) {
-        this.gameService.GetTables(id)
-          .subscribe(success => {
+    if (id != null) {
+      this.gameService.GetTables(id)
+        .subscribe(success => {
             console.log(success);
           },
           err => {
             console.log(err.error);
           });
-      }
+    }
   }
 }
--- src/app/services/web-socket.service.ts
+++ src/app/services/web-socket.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {webSocket} from 'rxjs/webSocket';
+import {CookieService} from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
@@ -7,14 +8,13 @@ import {webSocket} from 'rxjs/webSocket';
 export class WebSocketService {
   subject: any;
 
-  constructor() {
+  constructor(private cookieService: CookieService) {
     this.subject = webSocket('wss://localhost:5001/ws');
   }
 
   public sendToServer() {
     this.subject.subscribe();
-    this.subject.next('Hejsa');
+    this.subject.next(this.cookieService.get('session-token'));
     this.subject.complete();
   }
-
 }
