73888b9b3ea3e9cd339ca4f5bb8e38774e9c4b49
Mark Fog <mp@itoperators.dk>
2020-06-09 08:56:15 +0200

    Spaces

--- src/app/shared/services/web-socket.service.ts
+++ src/app/shared/services/web-socket.service.ts
@@ -23,7 +23,7 @@ export class WebSocketService {
     this.lobbyChatMessage = new BehaviorSubject<GameMessage>(null);
     this.tableChatMessage = new BehaviorSubject<GameMessage>(null);
     this.subject = webSocket('wss://ws.444.dk/ws');
-    //this.subject = webSocket('wss://localhost:5001/ws');
+    // this.subject = webSocket('wss://localhost:5001/ws');
     this.sendMessage(this.cookieService.get('session-token'));
 
     this.subject.subscribe(
